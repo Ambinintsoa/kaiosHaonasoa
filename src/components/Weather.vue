a387e05749a1b2193e8f5036c740a41c1553a559
Tonny <ynnotjoh@gmail.com>
2024-03-10 20:44:11 +0300

    apres modif toetr andro

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="card-game">
+    <div>{{ currentQuestion.weather }}</div>
     <div class="cards-container" id="cards-container">
-      <div v-for="(card, i) in cards" class="cards" :key="card" :tabindex="i">
+      <div
+        v-for="(card, i) in currentQuestion.choices"
+        :key="card"
+        :tabindex="i"
+        class="cards"
+      >
         <input type="checkbox" /><u>{{ card }}</u
         ><b></b>
       </div>
@@ -11,41 +17,61 @@
 
 <script>
 export default {
-  components: {},
   data() {
     return {
-      cardsToFind: ["1", "2", "3"],
-      cards: [],
-      cardsContainer: document.getElementById("card"),
+      questions: [
+        {
+          weather: "Fahavaratra",
+          choices: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
+          responses: ["1", "2", "3"],
+        },
+        {
+          weather: "Lohataona",
+          choices: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
+          responses: ["1", "2", "3"],
+        },
+        {
+          weather: "Lohataona 2",
+          choices: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
+          responses: ["1", "2", "3"],
+        },
+      ],
+      currentQuestionIndex: 0,
     };
   },
-  computed: {},
+  computed: {
+    currentQuestion() {
+      return this.questions[this.currentQuestionIndex];
+    },
+  },
   mounted() {
-    document.addEventListener("keydown", (evt) => {
-      this.handleKeydown(evt);
-    });
     this.initializeCards();
-    //le render ty ngamba hoe inna ny climat zao
     this.renderCards();
+    document.addEventListener("keydown", this.handleKeydown);
   },
   methods: {
     handleKeydown(evt) {
       var activeTab = document.querySelector("#opNav");
       if (activeTab.classList.contains("qTwo")) {
-        if (evt.key === "ArrowLeft") {
-          this.nav(-1);
-        } else if (evt.key === "ArrowRight") {
-          this.nav(1);
-        } else if (evt.key === "Enter") {
-          const focusedElement = document.activeElement;
-          if (focusedElement.classList.contains("cards")) {
-            this.clickOnCard(evt);
-          }
+        switch (evt.key) {
+          case "ArrowLeft":
+          case "ArrowRight":
+          case "ArrowUp":
+          case "ArrowDown":
+            this.nav(evt.key === "ArrowLeft" || evt.key === "ArrowUp" ? -1 : 1);
+            break;
+          case "Enter":
+            var focusedElement = document.activeElement;
+            if (focusedElement.classList.contains("cards")) {
+              this.clickOnCard(evt);
+            }
+            break;
+          default:
+            break;
         }
       }
     },
     nav(move) {
-      //   if (document.querySelector(".selectorMenuAntony.activeAntony")) {
       const currentIndex = document.activeElement.tabIndex;
       var next = currentIndex + move;
       next %= 9;
@@ -54,14 +80,10 @@ export default {
       if (targetElement) {
         targetElement.focus();
       }
-      //   }
     },
     initializeCards() {
-      this.cards = [];
-      for (let i = 0; i < 9; i++) {
-        this.cards.push(i + 1);
-      }
-      this.shuffle(this.cards);
+      this.questions.sort(() => Math.random() - 0.5);
+      this.shuffle(this.currentQuestion.choices);
     },
     shuffle(array) {
       for (let i = array.length - 1; i > 0; i--) {
@@ -74,15 +96,25 @@ export default {
       var cardElement = evt.target.childNodes[0];
       cardElement.checked = true;
       setTimeout(() => {
-        if (this.cardsToFind.includes(cardValue)) {
+        if (this.currentQuestion.responses.includes(cardValue)) {
           cardElement.classList.add("found");
-          this.cardsToFind.splice(this.cardsToFind.indexOf(cardValue), 1);
-          if (this.cardsToFind.length === 0) {
+          const remainingResponses = this.currentQuestion.responses.filter(
+            (response) => response !== cardValue
+          );
+          this.questions[
+            this.currentQuestionIndex
+          ].responses = remainingResponses;
+          if (this.currentQuestion.responses.length === 0) {
             alert(
-              "Félicitations ! Vous avez trouvé toutes les cartes à chercher."
+              "Félicitations ! Vous avez trouvé toutes les réponses pour cette question."
             );
-            this.initializeCards();
-            this.renderCards();
+            if (this.currentQuestionIndex === this.questions.length - 1) {
+              alert("Bravo ! Vous avez terminé le jeu.");
+              //tokony misy zavatra atao
+            } else {
+              this.currentQuestionIndex++;
+              this.renderCards();
+            }
           }
         } else {
           cardElement.checked = false;
@@ -94,15 +126,16 @@ export default {
       items.forEach((item) => {
         item.checked = false;
       });
+      this.shuffle(this.currentQuestion.choices);
     },
   },
 };
 </script>
+
 <style scoped>
-/* Conteneur de la table */
 #cards-container {
   display: flex;
-  height: 260px;
+  height: 200px;
   flex-wrap: wrap;
   flex-direction: row;
   justify-content: center;
@@ -115,7 +148,6 @@ export default {
 .cards:focus > b {
   box-shadow: 1px 1px 10px 1px black;
 }
-/* Styles pour le contenu de la carte */
 .cards > input,
 .cards > u,
 .cards > b {
@@ -124,13 +156,11 @@ export default {
   height: 60px;
   position: absolute;
 }
-
 .cards > input {
   z-index: 999;
   cursor: pointer;
   opacity: 0;
 }
-
 .cards > u,
 .cards > b {
   display: inline-block;
@@ -144,32 +174,24 @@ export default {
   -ms-user-select: none;
   user-select: none;
 }
-
-/* Face avant */
 .cards > u {
   background: #f8fff9;
   text-decoration: none;
   font: italic 12px Arial, Helvetica, sans-serif;
 }
-
 .cards > u > b {
   font: bold 14px "Comic Sans MS", cursive, sans-serif;
   color: teal;
   display: block;
 }
-
-/* Face arrière */
 .cards > b {
   background: linear-gradient(#fede1e, #dabb1e);
   border: 5px solid #f8fff9;
   box-sizing: border-box;
 }
-
-/* Animation et effets 3D */
 .cards {
   perspective: 1450px;
 }
-
 .cards > u,
 .cards > b {
   transition: all 0.8s;
@@ -177,42 +199,33 @@ export default {
   transform-style: preserve-3d;
   z-index: 0;
 }
-
 .cards > input:checked + u,
 .cards > input:checked + u + b {
   z-index: 100;
 }
-
 .cards > input:focus + u,
 .cards > input:focus + u + b {
   transition-property: transform, filter, box-shadow;
   outline: 0;
   z-index: 900;
 }
-
-/* Animation de la face avant */
 .cards > u {
   transform: translateX(160%) rotateY(-180deg);
   transform-origin: -30% center;
   box-shadow: 130px 30px 40px -20px rgba(0, 0, 0, 0);
 }
-
 .cards > input:checked + u {
   transform: translateX(0%) rotateY(0deg);
   box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.4);
 }
-
-/* Animation de la face arrière */
 .cards > b {
   transform: translateX(0%) rotateY(0deg);
   transform-origin: 130% center;
   box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.4);
 }
-
 .cards > input:checked + u + b {
   transform: translateX(-160%) rotateY(180deg);
 }
-
 .cards > u,
 .cards > b {
   cursor: pointer;
